7e3240885e3e4b6096a31902d76a372e73a362ec
chenjg4 <xk14chenjinhong@163.com>
2024-09-29 16:31:16 -0400

    EDA

--- util.py
+++ util.py
@@ -2,13 +2,19 @@
 
 import pandas as pd
 import numpy as np
+import os
+
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+import plotly.offline
+
 
 import kagglehub
 
-kagglehub.login()
+# kagglehub.login()
 
 
 def data_download(input_path,version=1):
@@ -17,11 +23,12 @@ def data_download(input_path,version=1):
     return output_path
 
 
-def read_csv(path,file=None):
-    if '.csv.gz' in file:
-        return pd.read_csv(path+'/'+file, compression='gzip')
+def read_csv(path):
+
+    if '.csv.gz' in path:
+        return pd.read_csv(path, compression='gzip')
     else:
-        return pd.read_csv(path+'/'+file)
+        return pd.read_csv(path)
 
 
 def read_folder(input_path,keywords = None):
@@ -38,7 +45,56 @@ def read_folder(input_path,keywords = None):
         return pd.concat(df_list)
     else:
         return pd.DataFrame()
-
     
 
+def create_folder(path):
+    os.makedirs(path,exist_ok=True)
+    print('folder created successfully:', path)
 
+    
+#### check uniuqe categorical values
+def check_unique(data_df):
+    for col in data_df.columns:
+        if data_df[col].dtype == 'object':
+            if data_df[col].nunique() < 10:
+                print('"'+col+'" has', data_df[col].nunique(),"unique values:\n", data_df[col].unique(),'\n')
+
+
+### drop unnessary columns:
+def drop_col(df, cols_list):
+    # defined_cols = [col for col in df.columns if col in cols_list]
+    # df.drop(defined_cols, axis=1, inplace=True) 
+    df.drop(cols_list, axis=1, inplace=True, errors='ignore')   ## only drop exist columns
+
+
+### violin plots
+def visualize_features_vs_target_label(df_data, label, feature_list, n_cols=3):
+    
+    if len(feature_list) % n_cols == 0:
+    
+        number_of_rows = int(len(feature_list)/n_cols)
+    else:
+        number_of_rows = int(len(feature_list)/n_cols) +1
+    
+    fig = make_subplots(rows=number_of_rows, cols=n_cols)
+    
+    row_pos = 1
+    col_pos = 1
+    
+    for feature_col in feature_list:
+
+        fig.add_trace(
+            go.Violin(x=df_data[label], y=df_data[feature_col], name=feature_col),
+            row=row_pos, col=col_pos
+        )
+        col_pos = col_pos + 1
+        
+        if col_pos > n_cols:
+            col_pos = 1
+            row_pos = row_pos + 1
+
+    fig.update_layout(#violingap=0, 
+                  #violinmode='overlay', 
+                  title_text="Features-to-Target Relations")
+
+    fig.show()
